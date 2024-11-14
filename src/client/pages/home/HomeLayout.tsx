2acdabed252e8782430f613d57b709890d8960ce
himanushi <himanushi@users.noreply.github.com>
2024-11-15 08:29:26 +0900

    HomeLayoutコンポーネントの簡易化

--- src/client/pages/home/HomeLayout.tsx
+++ src/client/pages/home/HomeLayout.tsx
@@ -1,26 +1,5 @@
-import {
-  IonContent,
-  IonHeader,
-  IonItemDivider,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
+import { Box } from "@yamada-ui/react";
 import { Me } from "~/client/features/me/Me";
 import { Todo } from "~/client/features/todo/Todo";
 
-export const HomeLayout = () => (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>Hono Todo</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent>
-      <IonItemDivider>User Setting</IonItemDivider>
-      <Me />
-      <IonItemDivider>Todo</IonItemDivider>
-      <Todo />
-    </IonContent>
-  </IonPage>
-);
+export const HomeLayout = () => <Box>aaa</Box>;
