016254baa7a7584c2769150245202175fb3b8d9e
himanushi <himanushi@users.noreply.github.com>
2024-11-18 18:02:50 +0900

    vite.config.tsの修正とvitest.config.tsの新規作成

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import path from "node:path";
-import {
-  defineWorkersConfig,
-  readD1Migrations,
-} from "@cloudflare/vitest-pool-workers/config";
 import pages from "@hono/vite-cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
 import adapter from "@hono/vite-dev-server/cloudflare";
+import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineWorkersConfig(async ({ mode }) => {
+export default defineConfig(async ({ mode }) => {
   if (mode === "client") {
     return {
       build: {
@@ -34,60 +30,24 @@ export default defineWorkersConfig(async ({ mode }) => {
       ],
     };
   }
-  if (mode === "server") {
-    return {
-      ssr: {
-        external: [
-          "react",
-          "react-dom",
-          "react-router-dom",
-          "@ionic/react",
-          "@ionic/react-router",
-          "@tanstack/react-query",
-        ],
-      },
-      plugins: [
-        pages(),
-        devServer({
-          adapter,
-          entry: "src/index.tsx",
-        }),
-        tsconfigPaths(),
-      ],
-      poolOptions: {
-        workers: {
-          miniflare: {},
-          wrangler: { configPath: "./wrangler.toml" },
-        },
-      },
-    };
-  }
-
-  // test
-  const migrationsPath = path.join(__dirname, "src", "db", "migrations");
-  const migrations = await readD1Migrations(migrationsPath);
 
   return {
-    test: {
-      setupFiles: ["./test/apply-migrations.ts"],
-      poolOptions: {
-        workers: {
-          singleWorker: true,
-          wrangler: {
-            configPath: "./wrangler.toml",
-          },
-          miniflare: {
-            bindings: { TEST_MIGRATIONS: migrations },
-          },
-        },
-      },
+    ssr: {
+      external: [
+        "react",
+        "react-dom",
+        "react-router-dom",
+        "@ionic/react",
+        "@ionic/react-router",
+        "@tanstack/react-query",
+      ],
     },
     plugins: [
+      pages(),
       devServer({
         adapter,
         entry: "src/index.tsx",
       }),
-      pages(),
       tsconfigPaths(),
     ],
     poolOptions: {
---
+++ vitest.config.ts
@@ -0,0 +1,45 @@
+import path from "node:path";
+import {
+  defineWorkersConfig,
+  readD1Migrations,
+} from "@cloudflare/vitest-pool-workers/config";
+import pages from "@hono/vite-cloudflare-pages";
+import devServer from "@hono/vite-dev-server";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineWorkersConfig(async () => {
+  const migrationsPath = path.join(__dirname, "src", "db", "migrations");
+  const migrations = await readD1Migrations(migrationsPath);
+
+  return {
+    test: {
+      setupFiles: ["./test/apply-migrations.ts"],
+      poolOptions: {
+        workers: {
+          singleWorker: true,
+          wrangler: {
+            configPath: "./wrangler.toml",
+          },
+          miniflare: {
+            bindings: { TEST_MIGRATIONS: migrations },
+          },
+        },
+      },
+    },
+    plugins: [
+      pages(),
+      devServer({
+        adapter,
+        entry: "src/index.tsx",
+      }),
+      tsconfigPaths(),
+    ],
+    poolOptions: {
+      workers: {
+        miniflare: {},
+        wrangler: { configPath: "./wrangler.toml" },
+      },
+    },
+  };
+});
