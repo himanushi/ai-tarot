3bf6f135aaf5c87a91f0ecec7c0329b7a10d917f
himanushi <himanushi@users.noreply.github.com>
2024-11-19 19:14:50 +0900

    読み込み機能の削除とカラーモードの変更機能の追加

--- src/client/pages/home/HomeLayout.tsx
+++ src/client/pages/home/HomeLayout.tsx
@@ -1,45 +1,13 @@
-import { Box, Button, Input, Wrap, useLoading } from "@yamada-ui/react";
-
-const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+import { Box, Button, Wrap, useColorMode } from "@yamada-ui/react";
 
 export const HomeLayout = () => {
-  const { screen, page, background } = useLoading();
-
-  const onLoadingScreen = async () => {
-    try {
-      screen.start();
-
-      await wait(5000);
-    } finally {
-      screen.finish();
-    }
-  };
-
-  const onLoadingPage = async () => {
-    try {
-      page.start();
-
-      await wait(5000);
-    } finally {
-      page.finish();
-    }
-  };
-
-  const onLoadingBackground = async () => {
-    try {
-      background.start();
-
-      await wait(5000);
-    } finally {
-      background.finish();
-    }
-  };
+  const { changeColorMode } = useColorMode();
 
   return (
     <Wrap gap="md">
-      <Button onClick={onLoadingScreen}>Start screen loading</Button>
-      <Button onClick={onLoadingPage}>Start page loading</Button>
-      <Button onClick={onLoadingBackground}>Start background loading</Button>
+      <Button onClick={() => changeColorMode("dark")}>
+        Start screen loading
+      </Button>
       <Box w="full" p="md" bg={["primary", "secondary"]} color="white">
         This is Box
       </Box>
