1f6714e67e4099a66eab7a391a22e2dbd5dc79ee
himanushi <himanushi@users.noreply.github.com>
2024-11-20 09:02:10 +0900

    コンテナ要素の追加

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { UIProvider, extendConfig } from "@yamada-ui/react";
+import { Container, UIProvider, extendConfig } from "@yamada-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -19,7 +19,9 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <UIProvider config={customConfig}>
-        <RouterProvider router={routes} />
+        <Container as="main" mx="auto" maxW="48em">
+          <RouterProvider router={routes} />
+        </Container>
       </UIProvider>
     </QueryClientProvider>
   );
