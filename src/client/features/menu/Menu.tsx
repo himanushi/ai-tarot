08579f1e8746f9e19f6b9bb27eeba1af71e85d6c
himanushi <himanushi@users.noreply.github.com>
2024-11-22 08:59:56 +0900

    メニュー表示の更新

--- src/client/features/menu/Menu.tsx
+++ src/client/features/menu/Menu.tsx
@@ -1,9 +1,16 @@
-import { Flex } from "@yamada-ui/react";
+import { Container, Flex, List, ListItem, VStack } from "@yamada-ui/react";
 
 export const Menu = () => {
   return (
-    <Flex as="nav" bgColor="amber.500">
-      Menu
-    </Flex>
+    <VStack as="nav" bgColor="amber.500">
+      <List>
+        <ListItem>
+          aaaa
+        </ListItem>
+        <ListItem>
+          aaaa
+        </ListItem>
+      </List>
+    </VStack>
   );
 };
