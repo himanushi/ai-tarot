155cbcd04856c8b6cd9d4f882be698ca032f957f
himanushi <himanushi@users.noreply.github.com>
2024-11-23 17:58:50 +0900

    タロット引き履歴APIの追加

---
+++ src/server/api/tarotDrawHistoryApi.ts
@@ -0,0 +1,36 @@
+import { zValidator } from "@hono/zod-validator";
+import { drizzle } from "drizzle-orm/d1";
+import { createFactory } from "hono/factory";
+import { z } from "zod";
+import { tarotDrawHistory } from "~/db/schema";
+import { authMiddleware } from "../utils/authMiddleware";
+import type { HonoPropsType } from "../utils/createApp";
+
+export const createTarotDrawHistoryApi =
+  createFactory<HonoPropsType>().createHandlers(
+    authMiddleware,
+    zValidator(
+      "query",
+      z.object({
+        question: z.string(),
+      }),
+    ),
+    async (c) => {
+      const me = c.get("me");
+      if (!me) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const question = c.req.query("question");
+      if (!question) {
+        return c.json({ error: "text is required" }, 400);
+      }
+
+      const db = drizzle(c.env.DB);
+      await db.insert(tarotDrawHistory).values({
+        userId: me.id,
+        question,
+        modelName: "gpt-4o",
+      });
+    },
+  );
