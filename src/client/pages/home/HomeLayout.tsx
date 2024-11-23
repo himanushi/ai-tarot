372f65009a4b74bfe3d54de3e4fe9a87398b1daf
himanushi <himanushi@users.noreply.github.com>
2024-11-23 17:58:59 +0900

    ゲームコンポーネントの追加

--- src/client/pages/home/HomeLayout.tsx
+++ src/client/pages/home/HomeLayout.tsx
@@ -1,15 +1,11 @@
 import { Stack, Text, VStack } from "@yamada-ui/react";
+import { Game } from "~/client/features/game/Game";
 import { Spread } from "~/client/features/spread/Spread";
 
 export const HomeLayout = () => {
   return (
     <VStack>
-      <Stack>
-        <Text>Home</Text>
-      </Stack>
-      <Stack>
-        <Spread />
-      </Stack>
+      <Game />
     </VStack>
   );
 };
