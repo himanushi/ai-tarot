909f73b59d85f2c74a2ae5024bec132c861f770f
himanushi <himanushi@users.noreply.github.com>
2024-11-23 17:27:37 +0900

    メニュー項目の更新

--- src/client/features/menu/Menu.tsx
+++ src/client/features/menu/Menu.tsx
@@ -1,15 +1,12 @@
-import { Container, Flex, List, ListItem, VStack } from "@yamada-ui/react";
+import { List, ListItem, VStack } from "@yamada-ui/react";
 
 export const Menu = () => {
   return (
     <VStack as="nav" bgColor="amber.500">
       <List>
-        <ListItem>
-          aaaa
-        </ListItem>
-        <ListItem>
-          aaaa
-        </ListItem>
+        <ListItem>占い</ListItem>
+        <ListItem>履歴</ListItem>
+        <ListItem>設定</ListItem>
       </List>
     </VStack>
   );
