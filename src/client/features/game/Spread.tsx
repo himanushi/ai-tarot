b60789a1b14017fa80b1bee4ac846d35f722e9f1
himanushi <himanushi@users.noreply.github.com>
2024-11-25 16:36:34 +0900

    TarotSpreadsApiへの変更

--- src/client/features/game/Spread.tsx
+++ src/client/features/game/Spread.tsx
@@ -1,23 +1,25 @@
 import { Button, Textarea, VStack, useAsyncCallback } from "@yamada-ui/react";
 import { hc } from "hono/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { clientUrl } from "~/client/utils/clientUrl";
-import type { TarotDrawHistoryApi } from "~/server/routes";
+import type { TarotSpreadsApi } from "~/server/routes";
 
-const query = hc<TarotDrawHistoryApi>(clientUrl);
+const query = hc<TarotSpreadsApi>(clientUrl);
 
 export const Spread = () => {
   const [question, setQuestion] = useState("");
   const nav = useNavigate();
 
-  const [isLoading, onClick] = useAsyncCallback(async () => {
-    const result = await query.api["tarot-draw-histories"].$post({
-      json: { question },
-    });
-    const body = await result.json();
-    if ("data" in body) nav(`/questions/${body.data.id}/spreads`);
-  }, [question, nav]);
+  useEffect(() => {}, []);
+
+  // const [isLoading, onClick] = useAsyncCallback(async () => {
+  //   const result = await query.api["tarot-draw-histories"].$post({
+  //     json: { question },
+  //   });
+  //   const body = await result.json();
+  //   if ("data" in body) nav(`/questions/${body.data.id}/spreads`);
+  // }, [question, nav]);
 
   return (
     <VStack>
