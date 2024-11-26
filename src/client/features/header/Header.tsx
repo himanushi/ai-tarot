4d46d8d1a743c754f06ffb0ded93047328f21bb0
himanushi <himanushi@users.noreply.github.com>
2024-11-27 06:49:36 +0900

    ヘッダーにナビゲーションメニューのトグル機能追加

--- src/client/features/header/Header.tsx
+++ src/client/features/header/Header.tsx
@@ -1,11 +1,75 @@
-import { Box, HStack, Heading } from "@yamada-ui/react";
+import { MenuIcon } from "@yamada-ui/lucide";
+import {
+  Box,
+  CloseButton,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  HStack,
+  Heading,
+  IconButton,
+  Spacer,
+  useDisclosure,
+} from "@yamada-ui/react";
+import { Menu } from "../menu/Menu";
 
 export const Header = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
     <Box as="header" bgColor="amber.500">
       <HStack>
         <Heading>占いのゲーム</Heading>
+        <Spacer />
+        {!isOpen ? (
+          <IconButton
+            variant="ghost"
+            aria-label="Open navigation menu"
+            color="muted"
+            display={{ base: "none", lg: "inline-flex" }}
+            icon={<MenuIcon fontSize="2xl" />}
+            _hover={{ bg: ["blackAlpha.50", "whiteAlpha.100"] }}
+            onClick={onOpen}
+          />
+        ) : (
+          <CloseButton
+            size="lg"
+            aria-label="Close navigation menu"
+            color="muted"
+            display={{ base: "none", lg: "inline-flex" }}
+            _hover={{ bg: ["blackAlpha.100", "whiteAlpha.50"] }}
+            onClick={onClose}
+          />
+        )}
       </HStack>
+      <Drawer
+        closeOnDrag
+        isFullHeight
+        isOpen={isOpen}
+        w="sm"
+        withCloseButton={false}
+        withDragBar={false}
+        onClose={onClose}
+      >
+        <DrawerHeader
+          fontSize="md"
+          fontWeight="normal"
+          justifyContent="flex-end"
+          pt="sm"
+        >
+          <CloseButton
+            size="lg"
+            aria-label="Close navigation menu"
+            color="muted"
+            display={{ base: "none", lg: "inline-flex" }}
+            _hover={{ bg: ["blackAlpha.100", "whiteAlpha.50"] }}
+            onClick={onClose}
+          />
+        </DrawerHeader>
+        <DrawerBody>
+          <Menu />
+        </DrawerBody>
+      </Drawer>
     </Box>
   );
 };
