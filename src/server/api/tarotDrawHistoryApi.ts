924e1ca0950013b8a64a78307667597eb5af74a2
himanushi <himanushi@users.noreply.github.com>
2024-11-26 19:19:00 +0900

    tarotDrawHistoryの名称変更とAPIの追加

--- src/server/api/tarotDrawHistoryApi.ts
+++ src/server/api/tarotDrawHistoryApi.ts
@@ -3,7 +3,7 @@ import { and, eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/d1";
 import { createFactory } from "hono/factory";
 import { z } from "zod";
-import { tarotDrawHistory } from "~/db/schema";
+import { tarotDrawHistories } from "~/db/schema";
 import { authMiddleware } from "../utils/authMiddleware";
 import type { HonoPropsType } from "../utils/createApp";
 
@@ -26,7 +26,7 @@ export const createTarotDrawHistoryApi =
 
       const db = drizzle(c.env.DB);
       const results = await db
-        .insert(tarotDrawHistory)
+        .insert(tarotDrawHistories)
         .values({
           userId: me.id,
           question,
@@ -42,9 +42,14 @@ export const patchSpreadIdTarotDrawHistoryApi =
   createFactory<HonoPropsType>().createHandlers(
     authMiddleware,
     zValidator(
+      "param",
+      z.object({
+        id: z.string().transform((v) => Number.parseInt(v, 10)),
+      }),
+    ),
+    zValidator(
       "json",
       z.object({
-        questionId: z.number(),
         spreadId: z.number(),
       }),
     ),
@@ -54,18 +59,95 @@ export const patchSpreadIdTarotDrawHistoryApi =
         return c.json({ error: "Unauthorized" }, 401);
       }
 
-      const { questionId, spreadId } = c.req.valid("json");
+      const { id } = c.req.valid("param");
+      const { spreadId } = c.req.valid("json");
 
       const db = drizzle(c.env.DB);
       await db
-        .update(tarotDrawHistory)
+        .update(tarotDrawHistories)
         .set({
           spreadId: spreadId,
         })
         .where(
           and(
-            eq(tarotDrawHistory.id, questionId),
-            eq(tarotDrawHistory.userId, me.id),
+            eq(tarotDrawHistories.id, id),
+            eq(tarotDrawHistories.userId, me.id),
+          ),
+        );
+
+      return c.json({ data: "ok" });
+    },
+  );
+
+export const shuffleDeckTarotDrawHistoryApi =
+  createFactory<HonoPropsType>().createHandlers(
+    authMiddleware,
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().transform((v) => Number.parseInt(v, 10)),
+      }),
+    ),
+    async (c) => {
+      const me = c.get("me");
+      if (!me) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const { id } = c.req.valid("param");
+
+      const db = drizzle(c.env.DB);
+
+      await db
+        .update(tarotDrawHistories)
+        .set({
+          deck: [],
+        })
+        .where(
+          and(
+            eq(tarotDrawHistories.id, id),
+            eq(tarotDrawHistories.userId, me.id),
+          ),
+        );
+
+      return c.json({ data: "ok" });
+    },
+  );
+
+export const fortuneTellingTarotDrawHistoryApi =
+  createFactory<HonoPropsType>().createHandlers(
+    authMiddleware,
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().transform((v) => Number.parseInt(v, 10)),
+      }),
+    ),
+    zValidator(
+      "json",
+      z.object({
+        spreadId: z.number(),
+      }),
+    ),
+    async (c) => {
+      const me = c.get("me");
+      if (!me) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const { id } = c.req.valid("param");
+      const { spreadId } = c.req.valid("json");
+
+      const db = drizzle(c.env.DB);
+      await db
+        .update(tarotDrawHistories)
+        .set({
+          spreadId: spreadId,
+        })
+        .where(
+          and(
+            eq(tarotDrawHistories.id, id),
+            eq(tarotDrawHistories.userId, me.id),
           ),
         );
 
