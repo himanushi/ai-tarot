4eb190ead7ea68a9a2c19af9a978252514fa1397
himanushi <himanushi@users.noreply.github.com>
2024-12-02 07:48:31 +0900

    ヘッダーコンポーネントのナビゲーション機能追加

--- src/client/features/header/Header.tsx
+++ src/client/features/header/Header.tsx
@@ -11,18 +11,26 @@ import {
   Spacer,
   useDisclosure,
 } from "@yamada-ui/react";
+import { useNavigate } from "react-router-dom";
 import { Menu } from "../menu/Menu";
 
 export const Header = () => {
   const { isOpen, onClose, onOpen } = useDisclosure();
+  const nav = useNavigate();
 
   return (
     <Box as="header" bgColor="amber.500">
       <HStack>
-        <Heading>占いのゲーム</Heading>
+        <Box padding="10px 20px">
+          <Heading cursor="pointer" onClick={() => nav("/")}>
+            占いのゲーム
+          </Heading>
+        </Box>
         <Spacer />
         {!isOpen ? (
           <IconButton
+            variant="ghost"
+            aria-label="Open navigation menu"
             color="muted"
             display={{ base: "none", lg: "inline-flex" }}
             icon={<MenuIcon fontSize="2xl" />}
